47bec2c5fa440afe6650824f35e496afcd533c61
dmesquita <d.mesquita285@gmail.com>
2018-04-19 13:52:43 -0400

    Correct naive column types given by pyspark and remove null values for string columns

---
+++ clean_data.py
@@ -0,0 +1,60 @@
+import os
+os.environ["PYSPARK_PYTHON"]='/Users/diogomesquita/anaconda/envs/py3.6/bin/python'
+
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, SparkSession
+from pyspark.sql.functions import UserDefinedFunction
+from pyspark.sql.types import StringType, IntegerType, DoubleType
+from pyspark.sql.functions import *
+
+import utils
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Clean data and remove null values.')
+parser.add_argument('files_in', metavar='fin', type=str, nargs='+',
+                    help='path to tsv files to clean')
+parser.add_argument('--verbose', metavar='v', type=bool, default=False,
+                    help='to print messages')
+
+def correct_bad_classified_cols(df, verbose=False):
+    replaced_df = df
+    for col, dtype in replaced_df.dtypes:
+        if 'string' in dtype:
+            tmp_col = "{}_temp".format(col)
+
+            replaced_df = replaced_df.withColumn(
+                tmp_col,
+                utils.replace_commas_symbols_udf(replaced_df[col]).cast(DoubleType()))
+
+            if utils.is_double(replaced_df, tmp_col):
+                replaced_df = replaced_df.withColumn(col, replaced_df[tmp_col])
+            replaced_df = replaced_df.drop(tmp_col)
+    return replaced_df
+
+def remove_na_string_cols(df, col):
+    string_cols = [col for col, dtype in df.dtypes if 'string' in dtype]
+    return df.dropna(subset=string_cols)
+
+def main(files_in, verbose):
+    spark = SparkSession \
+            .builder \
+            .appName("Nulls and Outliers Detection") \
+            .getOrCreate()
+
+    for tsv in files_in:
+        df = spark.read.csv(path = tsv, header = True,inferSchema = True, sep='\t')
+        replaced_df = correct_bad_classified_cols(df, verbose)
+
+        # remove null values
+        cleaned_df = remove_na_string_cols(replaced_df, col)
+        
+        #TODO: remove nas other types
+
+        f_out = "{}_clean".format(tsv.split('.')[0])
+        cleaned_df.coalesce(1).write.csv(f_out, mode='overwrite', sep='\t', header=True)
+        
+        spark.stop()
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(**vars(args))
---
+++ utils.py
@@ -0,0 +1,25 @@
+import re
+import numpy as np
+from pyspark.sql.functions import UserDefinedFunction
+from pyspark.sql.types import StringType
+
+commas_re = re.compile('(\d+,\d+)(,\d*)*')
+symbols_re = re.compile('(^\W+|\W+$)')
+
+def repl(m):
+    return "".join(m.group(0).split(','))
+
+def replace_commas_symbols(ele):
+    if not ele:
+        return 'None'
+
+    removed_commas = re.sub(commas_re, repl, ele)
+    # clean $ symbols and other symbols that are at the start or end of string
+    return re.sub(symbols_re, '', removed_commas)
+    
+def is_double(df, col):
+    vals = np.array(df.select(col).limit(100).rdd.map(lambda x: x[0]).collect())
+    not_double = np.sum(vals == None)/len(vals)
+    return not_double < .3
+
+replace_commas_symbols_udf = UserDefinedFunction(replace_commas_symbols, StringType())
