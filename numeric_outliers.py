63836a38f0fd4958f1d562f2c40111510e4f2910
Kenil Tanna <avidkenil@gmail.com>
2018-04-20 19:24:39 -0400

    changed the scaling for the get_outliers to 4

--- numeric_outliers.py
+++ numeric_outliers.py
@@ -5,7 +5,7 @@ def get_outliers(df,col):
     bounds = {}
     quantiles = df.approxQuantile(col,[0.25,0.75],0.05)
     IQR = quantiles[1] - quantiles[0]
-    bounds[col] = [quantiles[0] - 2 * IQR,quantiles[1] + 2 * IQR]
+    bounds[col] = [quantiles[0] - 4 * IQR,quantiles[1] + 4 * IQR]
     #print(bounds)
     if 'rid'not in df.columns:
         df = df.withColumn("rid", monotonically_increasing_id())
