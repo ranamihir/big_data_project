db269bf2c4429663fa347bf81e236f76c4c7a5ea
Kenil Tanna <avidkenil@gmail.com>
2018-04-20 22:45:30 -0400

    added dropna for above and below cleaning

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,4 @@
-import os
-import glob
-import shutil
-
+#import os
 #os.environ["PYSPARK_PYTHON"]='/Users/diogomesquita/anaconda/envs/py3.6/bin/python'
 
 from pyspark import SparkContext
@@ -22,6 +19,7 @@ parser.add_argument('--verbose', metavar='v', type=bool, default=False,
 
 def correct_bad_classified_cols(df, verbose=False):
     replaced_df = df
+    print(replaced_df.dtypes)
     for col, dtype in replaced_df.dtypes:
         if 'string' in dtype:
             tmp_col = "{}_temp".format(col)
@@ -29,14 +27,17 @@ def correct_bad_classified_cols(df, verbose=False):
             replaced_df = replaced_df.withColumn(
                 tmp_col,
                 utils.replace_commas_symbols_udf(replaced_df[col]).cast(DoubleType()))
-
+            #print(col,': after 1')
             if utils.is_double(replaced_df, tmp_col):
                 replaced_df = replaced_df.withColumn(col, replaced_df[tmp_col])
+                
+            #print('after 2')
             replaced_df = replaced_df.drop(tmp_col)
     return replaced_df
 
-def dropna(df):
-    return df.dropna()
+def remove_na_string_cols(df):
+    string_cols = [col for col, dtype in df.dtypes if 'string' in dtype]
+    return df.dropna(subset=string_cols)
 
 def main(files_in, verbose):
     spark = SparkSession \
@@ -45,23 +46,23 @@ def main(files_in, verbose):
             .getOrCreate()
 
     for tsv in files_in:
-        df = spark.read.csv(path = tsv, header = True,inferSchema = True, sep='\t')
-        replaced_df = correct_bad_classified_cols(df, verbose)
+        #df = spark.read.csv(path = tsv, header = True,inferSchema = True, sep='\t')
+        #df = spark.read.format("csv").option("header", "true").option("sep","\t").option("inferSchema", "true").load(tsv)
+        df = spark.read.load(tsv,format="csv", sep="\t", inferSchema="true", header="true")
+        #print('read file')
+        cleaned_df = remove_na_string_cols(df)
+        replaced_df = correct_bad_classified_cols(cleaned_df, verbose)
+        #print('after replaced file')
 
         # remove null values
-        cleaned_df = dropna(replaced_df)
+        cleaned_df = remove_na_string_cols(replaced_df)
         
         #TODO: remove nas other types
-        out_folder = "{}_clean".format(tsv.split('.')[0])
-        cleaned_df.coalesce(1).write.csv(out_folder, mode='overwrite', sep='\t', header=True)
 
-        csv_file = glob.glob("{}/*.csv".format(out_folder))[0]
-        f_out = "{}_clean.tsv".format(tsv.split('.')[0])
-        os.rename(csv_file, f_out)       
-        shutil.rmtree(out_folder)
-
+        f_out = "{}_clean".format(tsv.split('.')[0])
+        cleaned_df.coalesce(1).write.csv(f_out, mode='overwrite', sep='\t', header=True)
+        
         spark.stop()
-    
 if __name__ == '__main__':
     args = parser.parse_args()
     main(**vars(args))
